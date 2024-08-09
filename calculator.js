9803fdaaef1eb8ea757ee7c3bc20beb050de9678
Vad Colcev <vadimkolchev@gmail.com>
2024-08-09 10:19:48 +0300

    Add calculator operations

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+
+
+
+
 const calculator = {
     add: (num1, num2) => {
         return num1 + num2;
@@ -13,7 +17,34 @@ const calculator = {
 
     divide: (num1, num2) => {
         return num1 * num2;
+    },
+
+    memory: {
+        firstNum: 0,
+        secondNum: 0,
+        storage: 0,
+        operator,
+    },
+
+    operate: operate,
+
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+
     }
 }
 
-export { calculator };
+export { calculator, memory };
